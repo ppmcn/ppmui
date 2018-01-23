a74fec0b532d6bed66b1f5664144c213148a075f
Ma Junbao <mlive@live.cn>
2018-01-23 10:19:56 +0800

    gh-pages

--- gh-pages.sh
+++ gh-pages.sh
@@ -5,9 +5,9 @@ git push origin master
 git checkout gh-pages
 git checkout master -- site
 rm  *.html
-rm -rf ppmui _layout
+rm -rf ppmui _layout css
 mv -f site/* ./
-rm -rf size
+rm -rf _layout
 git add .
 git commit -am "gh-pages"
 git push origin gh-pages
